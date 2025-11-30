8a6dc909f82477d890725e5a09ea7ad34c3452ef
syng20 <samyng20@gmail.com>
2025-11-29 16:59:37 -0800

    can (redo later)

--- drivers/conanfile.py
+++ drivers/conanfile.py
@@ -22,5 +22,6 @@ class demos(ConanFile):
     python_requires_extend = "libhal-bootstrap.demo"
 
     def requirements(self):
-        self.requires("libhal-util/5.8.1")
-        self.requires("libhal-arm-mcu/1.19.6")
+        self.requires("libhal-util/[^5.4.0]")
+        self.requires("libhal-arm-mcu/[1.18.2 || latest]")
+        self.requires("minimp3/cci.20211201")
