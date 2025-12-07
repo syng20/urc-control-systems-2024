9a816ef024d79b44935c08980ef3f11a97939cf4
RaghavSareen2006 <raghavsareen2006@gmail.com>
2025-12-06 16:41:23 -0800

    homing

--- perseus/src/bldc_servo.cpp
+++ perseus/src/bldc_servo.cpp
@@ -233,10 +233,12 @@ void bldc_perseus::print_csv_format(float pTerm, float iTerm, float dTerm, float
 void bldc_perseus::homing()
 {
   auto homing = resources::homing_pin(); 
-  // volatile auto homing_level = homing.driver_level(); 
-  // while (homing_level != 0) homing_level = homing.driver_level();
-  m_h_bridge->power(0); // should this be handled by another function? 
-  // and the following too? 
+  volatile auto homing_level = homing->level(); 
+  while(homing_level != 0) 
+  {
+    homing_level = homing->level();
+  }
+  m_h_bridge->power(0); 
   // set "homed value" to current encoder value 
   home_encoder_value = m_encoder->read().angle;
 }
